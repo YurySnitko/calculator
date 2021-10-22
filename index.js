8d325dc07cc9be713b54d7d8eac54a085022a078
YurySnitko <yury.snitko@gmail.com>
2021-10-22 10:30:43 +0300

    added memory section, theme switcer

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const mcBtn = document.getElementById('mc');
 const mPlusBtn = document.getElementById('m+');
 const mMinusBtn = document.getElementById('m-');
 const mrBtn = document.getElementById('mr');
+const secondFuncBtn = document.getElementById('secondFunc');
+const change1Btn = document.getElementById('change1');
+const change2Btn = document.getElementById('change2');
+const theme1Btn = document.getElementById('theme1');
+const theme2Btn = document.getElementById('theme2');
 
 let value1 = '';
 let value2 = '';
@@ -15,6 +20,7 @@ let value3 = '';
 let operator = '';
 let lastAction = '';
 let memory = '0';
+let memoryAction = false;
 let doubleValueOperator = ['\u002B', '\u2212', 'xy', '\u00F7', '\u00D7', 'y\u221A'];
 let singleValueOperator = ['x2', 'x3', '1/x', '2\u221A', '3\u221A', 'ex', '10x', 'ln', 'log10'];
 
@@ -79,6 +85,9 @@ function getResult(a, b, op) {
         case '10x':
             res = Math.pow(10, a);
             break;
+        case '2x':
+            res = Math.pow(2, a);
+            break;
         case 'ln':
             if (a <= 0) {
                 res = 'Error'; 
@@ -91,6 +100,13 @@ function getResult(a, b, op) {
                 res = 'Error'; 
             } else {
                 res = Math.log10(a);
+            }
+            break;
+        case 'log2':
+            if (a <= 0) {
+                res = 'Error';
+            } else {
+                res = Math.log2(a);
             }
             break;
     }
@@ -129,6 +145,9 @@ calc.addEventListener('click', (event) => {
     } else if (lastAction === 'xy' || lastAction === 'y\u221A') {
         result.innerText += value;
         //value3 += value;
+    } else if (memoryAction) {
+        result.innerText = value;
+        memoryAction = false;
     } else {
         result.innerText += value;
     }
@@ -146,8 +165,8 @@ calc.addEventListener('click', (event) => {
     if (!event.target.classList.contains('equal')) {
         let active = document.querySelector('.active');
         if (active) active.classList.remove('active');
-        
         event.target.classList.add('active');
+
         if (value1 != 0 && lastAction !== '=') {
             value2 = result.innerText;
             value1 = result.innerText = getResult(value1, value2, operator );
@@ -157,21 +176,12 @@ calc.addEventListener('click', (event) => {
         lastAction = operator = event.target.innerText;
     }
 
-    if (event.target.classList.contains('equal')) {
-        // first variant
-
-        // if (lastAction !== '=') value2 = result.innerText;
-        // if (lastAction === "C") {
-        //     value1 = result.innerText
-        // } else {
-        //     value1 = result.innerText = getResult(value1, value2, operator);
-        //     lastAction = event.target.innerText;
-        // }   
+    if (event.target.classList.contains('equal')) { 
         if (doubleValueOperator.includes(lastAction) && lastAction === operator) {
             value2 = result.innerText;
             value1 = result.innerText = getResult(value1, value2, operator);
             lastAction = event.target.innerText;
-        } else if (operator && (lastAction === 'x2' || lastAction === 'x3')) {
+        } else if (operator && singleValueOperator.includes(lastAction)) {
             value2 = result.innerText;
             value1 = result.innerText = getResult(value1, value2, operator);
             lastAction = event.target.innerText;
@@ -179,25 +189,17 @@ calc.addEventListener('click', (event) => {
             value2 = result.innerText;
             value1 = result.innerText = getResult(value1, value2, operator);
             lastAction = event.target.innerText;
-        }
-        // if (lastAction === "C") {
-        //     value2 = result.innerText
-        // } else {
-        
-            
-        else if (lastAction !== operator && (lastAction === 'xy' || lastAction === 'y\u221A')) {
+        } else if (lastAction !== operator && (lastAction === 'xy' || lastAction === 'y\u221A')) {
             value3 = result.innerText;
             value2 = getResult(value2, value3, lastAction);
             value1 = result.innerText = getResult(value1, value2, operator);
             lastAction = event.target.innerText;
         } else if (lastAction !== operator && singleValueOperator.includes(lastAction)) {
             value1 = result.innerText = getResult(result.innerText, value2, lastAction);
-            //lastAction = event.target.innerText;
         } else {
             value1 = result.innerText = getResult(value1, value2, operator);
             lastAction = event.target.innerText;
-        }
-        //}    
+        }  
     }
 })
 
@@ -214,7 +216,6 @@ function clear() {
 }
 
 clearBtn.addEventListener('click', () => {
-    // todo: after first clear take .active back 
     clear();
     clearBtn.innerText = 'AC';
     lastAction = "C";
@@ -233,29 +234,16 @@ percent.addEventListener('click', () => {
     result.innerText = getResult(result.innerText, '100', '\u00F7')
 })
 
-// function pow2() {
-//     let a = result.innerText;
-//     let length = a.includes(',') ? a.slice(a.indexOf(',') + 1).length : 0;
-    
-//     a = result.innerText.includes(',') 
-//         ? Number(result.innerText.replace(',', '.')) 
-//         : Number(result.innerText);
-//     res = Math.pow(a, 2);
-//     res = length ? Math.round(res * Math.pow(10, length * 2)) / Math.pow(10, length * 2) : res;
-//     res = String(res);
-//     value1 = result.innerText = res.includes('.') ? res.replace('.', ',') : res;
-//     lastAction = operator = 'x2';
-// }
-
-//pow2Btn.addEventListener('click', pow2)
-
 calc.addEventListener('click', (event) => {
     if (!event.target.classList.contains('xy')) return;
 
     let active = document.querySelector('.active');
     if (active) active.classList.remove('active');
-
     event.target.classList.add('active');
+
+    // if (theme1Btn.checked) event.target.classList.add('calc__btn_light');
+    // if (theme2Btn.checked) event.target.classList.remove('calc__btn_light');
+
     // if (value1 != 0 && lastAction !== '=') {
     //     value2 = result.innerText;
     //     value1 = result.innerText = getResult(value1, value2, operator);
@@ -281,8 +269,92 @@ calc.addEventListener('click', (event) => {
 })
 
 mPlusBtn.addEventListener('click', () => {
-    memory === "0" ? memory = result.innerText : memory += result.innerText ;
+    memory === "0" ? memory = +result.innerText : memory += +result.innerText;
+    memoryAction = true;
+    if (theme1Btn.checked) mrBtn.classList.add('calc__btn_light');
+    if (theme2Btn.checked) mrBtn.classList.remove('calc__btn_light');
 })
 mMinusBtn.addEventListener('click', () => {
-    memory -= result.innerText;
+    memory === "0" ? memory = 0 - +result.innerText : memory -= +result.innerText;
+    memoryAction = true;
+    if (theme1Btn.checked) mrBtn.classList.add('calc__btn_light');
+    if (theme2Btn.checked) mrBtn.classList.remove('calc__btn_light');
 })
+mcBtn.addEventListener('click', () => {
+    memory = "0";
+    memoryAction = false;
+    if (theme1Btn.checked) mrBtn.classList.remove('calc__btn_light');
+    if (theme2Btn.checked) mrBtn.classList.add('calc__btn_light');
+})
+mrBtn.addEventListener('click', () => {
+    result.innerText = memory;
+    memoryAction = true;
+    if (theme1Btn.checked) mrBtn.classList.add('calc__btn_light');
+    if (theme2Btn.checked) mrBtn.classList.remove('calc__btn_light');
+})
+
+secondFuncBtn.addEventListener('click', () => {
+    if (change1Btn.innerText === '10x') {
+        change1Btn.innerHTML = "2<sup>x</sup>";
+        change2Btn.innerHTML = "log<sub>2</sub>";
+    } else {
+        change1Btn.innerHTML = "10<sup>x</sup>";
+        change2Btn.innerHTML = "ln";
+    }
+})
+
+calc.addEventListener('mousedown', (event) => {
+    if (!event.target.classList.contains('calc__btn_main')) return;
+    event.target.style.backgroundColor = "rgba(248, 205, 124, 0.959)";
+});
+calc.addEventListener('mouseup', (event) => {
+    if (!event.target.classList.contains('calc__btn_main')) return;
+    event.target.style.backgroundColor = '';
+});
+
+// calc.addEventListener('click', (event) => {
+//     if (event.target.classList.contains('calc__btn_main') 
+//         || event.target.classList.contains('calc__btn_num')
+//         || event.target.classList.contains('calc__theme')) return;
+//     event.target.style.backgroundColor = "#999";
+//     setTimeout(() => { event.target.style.backgroundColor = "" }, 100)
+// })
+
+function handleTheme() {
+    if (theme2Btn.checked) {
+        document.querySelector('.container').classList.add('container_light'); 
+        calc.classList.add('calc_light');
+        result.classList.add('display__result_light');
+        document.querySelector('.calc__theme').classList.add('calc__theme_light');
+
+        const calcBtnOther = document.querySelectorAll('.calc__btn_other');
+        for (let i = 0; i < calcBtnOther.length; i++) {
+            calcBtnOther[i].classList.add('calc__btn_light');
+        }
+        const calcBtnNum = document.querySelectorAll('.calc__btn_num');
+        for (let i = 0; i < calcBtnNum.length; i++) {
+            calcBtnNum[i].classList.add('calc__btn_num_light');
+        }
+
+        memoryAction ? mrBtn.classList.remove('calc__btn_light') : mrBtn.classList.add('calc__btn_light');
+    }
+    if (theme1Btn.checked) {
+        document.querySelector('.container').classList.remove('container_light');  
+        calc.classList.remove('calc_light');
+        result.classList.remove('display__result_light'); 
+        document.querySelector('.calc__theme').classList.remove('calc__theme_light');
+
+        const calcBtnOther = document.querySelectorAll('.calc__btn_other');
+        for (let i = 0; i < calcBtnOther.length; i++) {
+            calcBtnOther[i].classList.remove('calc__btn_light');
+        }
+        const calcBtnNum = document.querySelectorAll('.calc__btn_num');
+        for (let i = 0; i < calcBtnNum.length; i++) {
+            calcBtnNum[i].classList.remove('calc__btn_num_light');
+        }
+
+        memoryAction ? mrBtn.classList.add('calc__btn_light') : mrBtn.classList.remove('calc__btn_light');
+    }
+}
+
+document.getElementById('radioBtns').onclick = handleTheme;
